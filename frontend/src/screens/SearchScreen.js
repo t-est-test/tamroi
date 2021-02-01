4e408e137796717037b8414012974d536716ed14
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-02-01 17:29:20 +0700

    publish to github

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -70,9 +70,9 @@ export default function SearchScreen(props) {
               props.history.push(getFilterUrl({ order: e.target.value }));
             }}
           >
-            <option value="newest">Newest Arrivals</option>
+            {/* <option value="newest">Newest Arrivals</option> */}
             <option value="lowest">Price: Low to High</option>
-            <option value="highest">Price: High to Low</option>
+            {/* <option value="highest">Price: High to Low</option> */}
             <option value="toprated">Avg. Customer Reviews</option>
           </select>
         </div>
