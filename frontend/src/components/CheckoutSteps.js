a55da1c1193f82ec53eba984fea4eb75c9550e3e
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-02-01 22:13:32 +0700

    publish to github

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,12 +1,23 @@
+// import React from 'react';
+
+// export default function CheckoutSteps(props) {
+//   return (
+//     <div className="row checkout-steps">
+//       <div className={props.step1 ? 'active' : ''}>Sign-In</div>
+//       <div className={props.step2 ? 'active' : ''}>Shipping</div>
+//       <div className={props.step3 ? 'active' : ''}>Payment</div>
+//       <div className={props.step4 ? 'active' : ''}>Place Order</div>
+//     </div>
+//   );
+// }
 import React from 'react';
 
 export default function CheckoutSteps(props) {
-  return (
-    <div className="row checkout-steps">
-      <div className={props.step1 ? 'active' : ''}>Sign-In</div>
-      <div className={props.step2 ? 'active' : ''}>Shipping</div>
-      <div className={props.step3 ? 'active' : ''}>Payment</div>
-      <div className={props.step4 ? 'active' : ''}>Place Order</div>
-    </div>
-  );
+    return (
+        <div className="row checkout-steps">
+            <div className={props.step1 ? 'active' : ''}>เข้าสู่ระบบ</div>
+            <div className={props.step2 ? 'active' : ''}>ระบุข้อมูล</div>
+            <div className={props.step3 ? 'active' : ''}>เช็คอิน</div>
+        </div>
+    );
 }
