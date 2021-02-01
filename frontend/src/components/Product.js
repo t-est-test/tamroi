2f6c60be9d63bc711f4b0da4cab1443c7e08b3e2
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-02-02 02:36:16 +0700

    publish to github

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,7 +5,7 @@ import Rating from './Rating';
 export default function Product(props) {
   const { product } = props;
   return (
-    <div key={product._id} className="card">
+    <div key={product._id} className="cardhome">
       <Link to={`/product/${product._id}`}>
         <img className="medium" src={product.image} alt={product.name} />
       </Link>
