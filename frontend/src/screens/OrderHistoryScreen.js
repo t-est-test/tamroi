b567a2cc681622b131ee579e84b644ffc073b3ec
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-02-01 22:07:39 +0700

    publish to github

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -22,26 +22,26 @@ export default function OrderHistoryScreen(props) {
         <table className="table">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>DATE</th>
+              <th>วันที่เช็คอิน</th>
+              {/* <th>DATE</th>
               <th>TOTAL</th>
               <th>PAID</th>
-              <th>DELIVERED</th>
-              <th>ACTIONS</th>
+              <th>DELIVERED</th> */}
+              <th>รายละเอียด</th>
             </tr>
           </thead>
           <tbody>
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
-                <td>{order.createdAt.substring(0, 10)}</td>
+                {/* <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
                 <td>
                   {order.isDelivered
                     ? order.deliveredAt.substring(0, 10)
                     : 'No'}
-                </td>
+                </td> */}
                 <td>
                   <button
                     type="button"
