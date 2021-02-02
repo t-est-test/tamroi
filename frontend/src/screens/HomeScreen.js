7663e4c85dbaac82d76dbd472f43d0762bca869f
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-02-02 20:14:39 +0700

    publish to github

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,6 +10,15 @@ import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
 
 export default function HomeScreen() {
+  const {
+    name = 'all',
+    category = 'all',
+    min = 0,
+    max = 0,
+    rating = 0,
+    order = 'newest',
+    pageNumber = 1,
+  } = useParams();
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
@@ -20,6 +29,17 @@ export default function HomeScreen() {
     error: errorSellers,
     users: sellers,
   } = userTopSellersList;
+
+  const getFilterUrl = (filter) => {
+    const filterPage = filter.page || pageNumber;
+    const filterCategory = filter.category || category;
+    const filterName = filter.name || name;
+    const filterRating = filter.rating || rating;
+    const sortOrder = filter.order || order;
+    const filterMin = filter.min ? filter.min : filter.min === 0 ? 0 : min;
+    const filterMax = filter.max ? filter.max : filter.max === 0 ? 0 : max;
+    return `/search/category/${filterCategory}/name/${filterName}/min/${filterMin}/max/${filterMax}/rating/${filterRating}/order/${sortOrder}/pageNumber/${filterPage}`;
+  };
 
   useEffect(() => {
     dispatch(listProducts({}));
@@ -56,6 +76,20 @@ export default function HomeScreen() {
       ) : (
         <>
           {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div>
+          จัดเรียงตาม{' '}
+          <select
+            value={order}
+            onChange={(e) => {
+              props.history.push(getFilterUrl({ order: e.target.value }));
+            }}
+          >
+            <option value="newest">สถานที่ใหม่ล่าสุด</option>
+            <option value="lowest">เก่า - ใหม่</option>
+            {/* <option value="highest">Price: High to Low</option> */}
+            {/* <option value="toprated">Avg. Customer Reviews</option> */}
+          </select>
+        </div>
           <div className="row center">
             {products.map((product) => (
               <Product key={product._id} product={product}></Product>
