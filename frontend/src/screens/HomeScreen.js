7d396e3ff97d3e431ebc460c51c22a3c406f3a4f
tumnaja727 <chairatchangtongdaeng30222@gmail.com>
2021-03-11 09:13:46 +0700

    publish to github

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -40,8 +40,8 @@ export default function HomeScreen() {
               <div key={seller._id}>
                 <Link to={`/seller/${seller._id}`}>
                   <img src={seller.seller.logo} />
-                  <img src={seller.seller.logo,seller.seller.logo1}  alt={seller.seller.name} />
-                  <p className="legend">{seller.seller.name}</p>
+                  {/* <img src={seller.seller.logo,seller.seller.logo1}  alt={seller.seller.name} />
+                  <p className="legend">{seller.seller.name}</p> */}
                 </Link>
               </div>
             ))}
